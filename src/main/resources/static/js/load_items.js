f9751f2bb617830aed21921d5a134f0abb9f8d01
Kirill <kpicmo@mail.ru>
2024-02-14 14:33:58 +0300

    fixed buds added html all warking

--- src/main/resources/static/js/load_items.js
+++ src/main/resources/static/js/load_items.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", load_items())
-card_container = document.getElementsByClassName("container")[0];
+card_container = document.getElementById("popular");
+console.log(card_container)
 console.log(card_container);
 
 async function get_all() {
@@ -14,17 +15,13 @@ function create_card(id, img, name, price) {
     console.log(img, name, price);
     const card_item = 
     `
-        <div class="card">
-            <div class="card_top">
-                <a href="alcohols/${id}" class="card_img">
-                    <img class="card_img" src="data:image/png;base64,${img}" alt="beluga" width="220px">
-                </a>
-            </div>
-            <div class="card_bottom">
-                <div class="card_price">${price}</div>
-                <a href="#" class="card_title">${name}</a>
-                <a href="#" class="card_btn">В корзину</a>
-            </div>
+        <div class="product_card">
+            <a href="alcohols/${id}">
+                <img src="data:image/png;base64,${img}" alt="">
+            </a>
+            <span class="name">${name}</span>
+            <span class="price">${price}</span>
+            <button class="add_to_card_btn">add to cart</button>
         </div>
     `
     card_container.insertAdjacentHTML("afterbegin", card_item)
