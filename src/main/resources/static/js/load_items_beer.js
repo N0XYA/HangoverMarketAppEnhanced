040512afa0aafeb945bacefe8b57a2a8495381ab
Kirill <kpicmo@mail.ru>
2024-02-14 17:50:56 +0300

    done + need to remake js CHECK THIS COMMIT

--- src/main/resources/static/js/load_items_beer.js
+++ src/main/resources/static/js/load_items_beer.js
@@ -1,9 +1,7 @@
 window.addEventListener("load", load_items())
-card_container = document.getElementsByClassName("container")[0];
-console.log(card_container);
+card_container = document.getElementById("popular");
 
 async function get_all_beer() {
-    console.log("Ready!");
     const response = await fetch("/alcohols/get_all_beer");
 
     var data = await response.json();
@@ -11,22 +9,31 @@ async function get_all_beer() {
 }
 
 function create_card(id, img, name, price) {
-    console.log(img, name, price);
     const card_item =
     `
-        <div class="card">
-            <div class="card_top">
-                <a href="${id}" class="card_img">
-                    <img class="card_img" src="data:image/png;base64,${img}" alt="beluga" width="220px">
-                </a>
-            </div>
-            <div class="card_bottom">
-                <div class="card_price">${price}</div>
-                <a href="#" class="card_title">${name}</a>
-                <a href="#" class="card_btn">В корзину</a>
-            </div>
+        <div class="product_card">
+            <a href="${id}">
+                <img src="data:image/png;base64,${img}" alt="">
+            </a>
+            <span class="name">${name}</span>
+            <span class="price">${price}</span>
+            <button class="add_to_card_btn">add to cart</button>
         </div>
     `
+    // `
+    //     <div class="card">
+    //         <div class="card_top">
+    //             <a href="${id}" class="card_img">
+    //                 <img class="card_img" src="data:image/png;base64,${img}" alt="beluga" width="220px">
+    //             </a>
+    //         </div>
+    //         <div class="card_bottom">
+    //             <div class="card_price">${price}</div>
+    //             <a href="#" class="card_title">${name}</a>
+    //             <a href="#" class="card_btn">В корзину</a>
+    //         </div>
+    //     </div>
+    // `
     card_container.insertAdjacentHTML("afterbegin", card_item)
 }
 
