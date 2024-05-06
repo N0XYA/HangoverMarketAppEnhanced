45793cd03b95acafb85d63c7cded02cb6a7f4a36
Kirill "Windflaw" Valikov <93864873+Windflawoffical@users.noreply.github.com>
2024-05-06 20:43:04 +0300

    Update front

--- src/main/resources/static/js/load_items_sparkling.js
+++ src/main/resources/static/js/load_items_sparkling.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", load_items())
-card_container = document.getElementsByClassName("container")[0];
+card_container = document.getElementById("popular");
 console.log(card_container);
 
 async function get_all_sparkling() {
@@ -14,19 +14,15 @@ function create_card(id, img, name, price) {
     console.log(img, name, price);
     const card_item =
     `
-        <div class="card">
-            <div class="card_top">
-                <a href="${id}" class="card_img">
-                    <img class="card_img" src="data:image/png;base64,${img}" alt="beluga" width="220px">
+            <div class="product_card">
+                <a href="${id}">
+                    <img src="data:image/png;base64,${img}" alt="">
                 </a>
+                <span class="name">${name}</span>
+                <span class="price">${price}</span>
+                <button class="add_to_card_btn">add to cart</button>
             </div>
-            <div class="card_bottom">
-                <div class="card_price">${price}</div>
-                <a href="#" class="card_title">${name}</a>
-                <a href="#" class="card_btn">В корзину</a>
-            </div>
-        </div>
-    `
+        `
     card_container.insertAdjacentHTML("afterbegin", card_item)
 }
 
